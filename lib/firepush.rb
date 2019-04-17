4f38bfaaf06581c63ee2cb4bb19186e4f7bb4149
mmyoji <mmyoji@gmail.com>
2019-04-17 17:00:32 +0900

    Stop using difficult autoload

--- lib/firepush.rb
+++ lib/firepush.rb
@@ -1,25 +1,20 @@
 # frozen_string_literal: true
 
-module Firepush
-  autoload :Client,       "firepush/client"
-  autoload :Message,      "firepush/message"
-  autoload :MessageTypes, "firepush/message_types"
+require "firepush/helper_methods"
 
-  module MessageType
-    autoload :Base,         "firepush/message_type/base"
-    autoload :Builder,      "firepush/message_type/builder"
-    autoload :Data,         "firepush/message_type/data"
-    autoload :Notification, "firepush/message_type/notification"
-  end
+require "firepush/recipient/base"
+require "firepush/recipient/condition"
+require "firepush/recipient/token"
+require "firepush/recipient/topic"
+require "firepush/recipient/builder"
 
-  module Recipient
-    autoload :Base,      "firepush/recipient/base"
-    autoload :Builder,   "firepush/recipient/builder"
-    autoload :Condition, "firepush/recipient/condition"
-    autoload :Token,     "firepush/recipient/token"
-    autoload :Topic,     "firepush/recipient/topic"
-  end
+require "firepush/message_type/base"
+require "firepush/message_type/data"
+require "firepush/message_type/notification"
+require "firepush/message_type/builder"
 
-  autoload :HelperMethods, "firepush/helper_methods"
-  autoload :Version,       "firepush/version"
-end
+require "firepush/message_types"
+require "firepush/message"
+require "firepush/client"
+
+require "firepush/version"
